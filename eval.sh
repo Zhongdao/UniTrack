0de6b6bcdfe1a93a7c9a2d883ec3e29090ab5bdd
Zhongdao <zhongdwang@gmail.com>
2021-07-07 18:27:44 +0800

    clean up sot part

--- eval.sh
+++ eval.sh
@@ -10,9 +10,9 @@ EXP_NAME=$1
 CFG_PATH=config/${EXP_NAME}.yaml
 SMRY_ROOT=results/summary/${EXP_NAME}
 mkdir -p $SMRY_ROOT
-#CUDA_VISIBLE_DEVICES=$2 python -u test/test_sot_siamfc.py --config $CFG_PATH | tee results/summary/${EXP_NAME}/sot_siamfc.log 2>&1 
-#CUDA_VISIBLE_DEVICES=$2 python -u test/test_sot_cfnet.py --config $CFG_PATH | tee results/summary/${EXP_NAME}/sot_cfnet.log 2>&1
+CUDA_VISIBLE_DEVICES=$2 python -u test/test_sot_siamfc.py --config $CFG_PATH | tee results/summary/${EXP_NAME}/sot_siamfc.log 2>&1 
+CUDA_VISIBLE_DEVICES=$2 python -u test/test_sot_cfnet.py --config $CFG_PATH | tee results/summary/${EXP_NAME}/sot_cfnet.log 2>&1
 CUDA_VISIBLE_DEVICES=$2 python -u test/test_vos.py --config $CFG_PATH | tee results/summary/${EXP_NAME}/vos.log 2>&1
-#CUDA_VISIBLE_DEVICES=$2 python -u test/test_mot.py --config $CFG_PATH | tee results/summary/${EXP_NAME}/mot.log 2>&1
-#CUDA_VISIBLE_DEVICES=$2 python -u test/test_mots.py --config $CFG_PATH | tee  results/summary/${EXP_NAME}/mots.log 2>&1
-#CUDA_VISIBLE_DEVICES=$2 python -u test/test_posetrack.py --config $CFG_PATH | tee results/summary/${EXP_NAME}/posetrack.log 2>&1
+CUDA_VISIBLE_DEVICES=$2 python -u test/test_mot.py --config $CFG_PATH | tee results/summary/${EXP_NAME}/mot.log 2>&1
+CUDA_VISIBLE_DEVICES=$2 python -u test/test_mots.py --config $CFG_PATH | tee  results/summary/${EXP_NAME}/mots.log 2>&1
+CUDA_VISIBLE_DEVICES=$2 python -u test/test_posetrack.py --config $CFG_PATH | tee results/summary/${EXP_NAME}/posetrack.log 2>&1
---
+++ tracker/sot/lib/models/__init__.py
@@ -0,0 +1,6 @@
+from .cfnet import CFNet
+from .siamfc import SiamFC
+from .connect import box_tower, AdjustLayer, AlignHead, Corr_Up, MultiDiCorr, OceanCorr
+from .backbones import ResNet50, ResNet22W
+from .modules import MultiFeatureBase
+
