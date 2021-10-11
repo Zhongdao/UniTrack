1bc4694061f16103fe6b4dbd6600b9ce3f42b8eb
Zhongdao <zhongdwang@gmail.com>
2021-10-11 17:01:08 +0800

    [FIX] pose track bug

--- tracker/mot/multitracker.py
+++ tracker/mot/multitracker.py
@@ -1,33 +1,26 @@
-import os
-import pdb
-import cv2
-import time
-import itertools
-import os.path as osp
 from collections import deque
 
-import numpy as np
 import torch
-import torch.nn.functional as F
 from torchvision import ops
 
-from model import AppearanceModel,partial_load
+from model import AppearanceModel
 from utils.log import logger
 from core.association import matching
 from core.propagation import propagate
 from core.motion.kalman_filter import KalmanFilter
 
-from utils.box import * 
+from utils.box import *
 from utils.mask import *
-from .basetrack import *
+from .basetrack import sub_stracks, joint_stracks, remove_duplicate_stracks, \
+                       STrack, TrackState
 
 
 class AssociationTracker(object):
     def __init__(self, opt):
         self.opt = opt
-        self.tracked_stracks = []  # type: list[STrack]
-        self.lost_stracks = []     # type: list[STrack]
-        self.removed_stracks = []  # type: list[STrack]
+        self.tracked_stracks = []
+        self.lost_stracks = []
+        self.removed_stracks = []
 
         self.frame_id = 0
         self.det_thresh = opt.conf_thres
@@ -38,11 +31,11 @@ class AssociationTracker(object):
 
         self.app_model = AppearanceModel(opt).to(opt.device)
         self.app_model.eval()
-        
+
         if not self.opt.asso_with_motion:
             self.opt.motion_lambda = 1
             self.opt.motion_gated = False
-        
+
     def extract_emb(self, img, obs):
         raise NotImplementedError
 
@@ -56,13 +49,12 @@ class AssociationTracker(object):
         refind_stracks = []
         lost_stracks = []
         removed_stracks = []
- 
-        t1 = time.time()
+
         detections = self.prepare_obs(img, img0, obs)
 
         ''' Add newly detected tracklets to tracked_stracks'''
         unconfirmed = []
-        tracked_stracks = []  # type: list[STrack]
+        tracked_stracks = []  
         for track in self.tracked_stracks:
             if not track.is_activated:
                 unconfirmed.append(track)
@@ -72,12 +64,13 @@ class AssociationTracker(object):
         ''' Step 2: First association, with embedding'''
         tracks = joint_stracks(tracked_stracks, self.lost_stracks)
         dists, recons_ftrk = matching.reconsdot_distance(tracks, detections)
-        if self.opt.use_kalman: 
+        if self.opt.use_kalman:
             # Predict the current location with KF
             STrack.multi_predict(tracks)
-            dists = matching.fuse_motion(self.kalman_filter, dists, tracks, detections, 
-                    lambda_=self.opt.motion_lambda, gate=self.opt.motion_gated)
-        if obs.shape[1] == 6:
+            dists = matching.fuse_motion(self.kalman_filter, dists, tracks, detections,
+                                         lambda_=self.opt.motion_lambda,
+                                         gate=self.opt.motion_gated)
+        if hasattr(obs, 'shape') and obs.shape[1] == 6:
             dists = matching.category_gate(dists, tracks, detections)
         matches, u_track, u_detection = matching.linear_assignment(dists, thresh=0.7)
 
@@ -90,14 +83,14 @@ class AssociationTracker(object):
             else:
                 track.re_activate(det, self.frame_id, new_id=False)
                 refind_stracks.append(track)
-        
+
         if self.opt.use_kalman:
             '''(optional) Step 3: Second association, with IOU'''
-            tracks = [tracks[i] for i in u_track if tracks[i].state==TrackState.Tracked]
+            tracks = [tracks[i] for i in u_track if tracks[i].state == TrackState.Tracked]
             detections = [detections[i] for i in u_detection]
             dists = matching.iou_distance(tracks, detections)
             matches, u_track, u_detection = matching.linear_assignment(dists, thresh=0.5)
-            
+
             for itracked, idet in matches:
                 track = tracks[itracked]
                 det = detections[idet]
@@ -127,7 +120,6 @@ class AssociationTracker(object):
                 track.mark_lost()
                 lost_stracks.append(track)
 
-
         """ Step 4: Init new stracks"""
         for inew in u_detection:
             track = detections[inew]
@@ -155,8 +147,3 @@ class AssociationTracker(object):
         # get scores of lost tracks
         output_stracks = [track for track in self.tracked_stracks if track.is_activated]
         return output_stracks
-
-
-
-        
-
